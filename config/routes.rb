11442a46422eff111033b7069d5a1f3ee7c5bb4e
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-01-27 21:16:06 -0500

    configured client side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
+
+  get '*path',
+  to: 'fallback#index',
+  constraints: ->(req) { !req.xhr? && req.format.html? }
 end
