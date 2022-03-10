271795ee00489b75e454a62367f239c843c1481a
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-03-10 11:47:18 -0500

    added search bar

--- client/src/Components/Mountains.js
+++ client/src/Components/Mountains.js
@@ -1,16 +1,35 @@
+import { useState } from 'react'
 import Mountain from './Mountain'
 
 function Mountains ({ addToFavorites, mountainsArray }) {
+
+    const [searchTerm, setSearchTerm] = useState("")
 
     function mountainToMountains (clickedMountain) {
         addToFavorites(clickedMountain)
     }
 
+    function handleChange(e) {
+        setSearchTerm(e.target.value)
+    }
+
     return (
 
         <div className='mountains'>
+            <input
+            type="text"
+            onChange={handleChange}
+            />
             <br></br>
-            {mountainsArray.map((mountain) => {
+            {mountainsArray.filter((mountain) => {
+                if (searchTerm == "") {
+                    return mountain;
+                }
+                else if (mountain.name.toLowerCase().includes(searchTerm.toLowerCase()) || mountain.pass_info.toLowerCase().includes(searchTerm.toLowerCase()) || mountain.nearest_airport.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return mountain;
+                }
+            })
+            .map((mountain) => {
             return (
                 <Mountain
                 key={mountain.id}
